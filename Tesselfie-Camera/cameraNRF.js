981969c17d6cb283d211dbc148c9fe2224ad86e3
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-19 16:14:01 -0700

    Fixes NRF and adds microSD

--- Tesselfie-Camera/cameraNRF.js
+++ Tesselfie-Camera/cameraNRF.js
@@ -22,36 +22,16 @@ nrf.on('ready', function () {
     nrf.printDetails();
   }, 5000);
 
-  if (role === 'ping') {
-    console.log("PING out");
-
-    tx = nrf.openPipe('tx', pipes[0], {autoAck: false}); // transmit address F0F0F0F0D2
-    var rx = nrf.openPipe('rx', pipes[1], {size: 4}); // receive address F0F0F0F0D2
-    
-    tx.on('ready', function () {
-      pingOpen = true;
-
-      console.log('pipe is open');
-      var n = 0;
-      var testObj = {
-        machine: 'Tessel',
-        color: 'red'
-      };
-    });
-    rx.on('data', function (d) {
-      console.log("Got response back:", d);
-    });
-  } else {
-    console.log("PONG back");
-    var rx = nrf.openPipe('rx', pipes[0], {size: 32});  
-      tx = nrf.openPipe('tx', pipes[1], {autoAck: false}); 
-    rx.on('data', function (d) {
-      console.log("Got data, will respond", d.toString());
-      tx.write(d);
-    });
-    tx.on('error', function (e) {
-      console.warn("Error sending reply.", e);
-    });
-  }
+  console.log("PONG back");
+  var rx = nrf.openPipe('rx', pipes[0], {size: 32});  
+    tx = nrf.openPipe('tx', pipes[1], {autoAck: false}); 
+  rx.on('data', function (d) {
+    console.log("Got data, will respond", d.toString());
+    tx.write(d);
+  });
+  tx.on('error', function (e) {
+    console.warn("Error sending reply.", e);
+  });
+  
   console.log('kaleover');
 });
